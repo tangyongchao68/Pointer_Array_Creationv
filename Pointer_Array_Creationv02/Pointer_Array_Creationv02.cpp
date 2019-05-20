a2c4b2ca18323f6dd61d7f2e6dea98ff93802e09
tangyongchao <tangyongchao68@163.com>
2019-05-20 11:26:01 +0800

    Annotations were added

--- Pointer_Array_Creationv02/Pointer_Array_Creationv02.cpp
+++ Pointer_Array_Creationv02/Pointer_Array_Creationv02.cpp
@@ -2,7 +2,7 @@
 //
 
 #include "stdafx.h"
-#include<iostream>
+#include<iostream>                 //这个头文件是输入输出的，input and output stream.
 
 using namespace std;
 int main()
@@ -10,7 +10,7 @@ int main()
 	int num;
 	cout << "输入一个数：" << "\n";
 	cin >> num;
-	int *arrays = new int[num];
+	int *arrays = new int[num];        //new运算符根据输入值计算要分配的内存空间大小
 	for (int i = 0; i < num; i++)
 	{
 		arrays[i] = i + 1;
@@ -20,7 +20,7 @@ int main()
 		cout << arrays[i] << "\t";
 	}
 	cout << "\n";
-	delete[]arrays;
+	delete[]arrays;                  //delete运算符后面有一个括号，说明要删除的是一个数组
     return 0;
 }
 
